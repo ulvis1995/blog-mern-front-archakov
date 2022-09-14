6840788f113a0b09b7226328a9babd196880680a
ViolettaParamonova <violettapar1995@gmail.com>
2022-09-14 13:28:15 +0300

    3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ export const Home = () => {
           : (<Post
               id={obj._id} key={index}
               title={obj.title}
-              imageUrl={obj.imageUrl ? `process.env.REACT_APP_API_URL/${obj.imageUrl}` : ''}
+              imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
               user={obj.user}
               createdAt={obj.createdAt}
               viewsCount={obj.viewsCount}
