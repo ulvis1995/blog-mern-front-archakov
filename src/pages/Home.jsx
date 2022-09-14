e2b2a877b3591ee48d7032de0930c50427d84c9e
ViolettaParamonova <violettapar1995@gmail.com>
2022-09-14 12:16:06 +0300

    2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => 
+          {(posts.items).map((obj, index) => 
           isPostsLoading 
           ? (<Post key={index} isLoading={true}/>)
           : (
