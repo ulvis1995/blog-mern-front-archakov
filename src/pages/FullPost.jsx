1fd134d44a492c8ed38f11e170fb532cef107c4f
ViolettaParamonova <violettapar1995@gmail.com>
2022-09-14 12:04:41 +0300

    first

---
+++ src/pages/FullPost.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { Post } from "../components/Post";
+import { Index } from "../components/AddComment";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { useParams } from "react-router-dom";
+import axios from "../axios";
+import ReactMarkdown from "react-markdown";
+
+export const FullPost = () => {
+  const [data, setData] = React.useState();
+  const [isLoading, setLoading] = React.useState(true);
+  const {id} = useParams();
+
+  React.useEffect(() => {
+    axios.get(`/posts/${id}`)
+    .then( res => {
+      setData(res.data);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.warn(error);
+      alert('Ошибка при получении статьи');
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} />
+  }
+
+  return (
+    <>
+      <Post
+        id={data._id}
+        title={data.title}
+        imageUrl={data.imageUrl ? `http://localhost:4444${data.imageUrl}` : ''}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
+        commentsCount={3}
+        tags={data.tags}
+        isEditable
+      >
+      <ReactMarkdown children={data.text} />
+      </Post>
+      <CommentsBlock
+        items={[
+          {
+            user: {
+              fullName: "Вася Пупкин",
+              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+            },
+            text: "Это тестовый комментарий 555555",
+          },
+          {
+            user: {
+              fullName: "Иван Иванов",
+              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+            },
+            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+          },
+        ]}
+        isLoading={false}
+      >
+        <Index />
+      </CommentsBlock>
+    </>
+  );
+};
